2417d3354a35709bb2b1e5bdd1e318b0c0d08eef
Francesco <francesco.ponzin@gmail.com>
2020-05-01 12:49:13 +0200

    add some flowers

--- config/schema/flowers.sql
+++ config/schema/flowers.sql
@@ -17,3 +17,24 @@ CREATE TABLE `flowers` (
     `modified` datetime,
     FOREIGN KEY (`occasion_id`) REFERENCES `occasions` (`id`)  ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+INSERT INTO `flowers`
+(`name`,`occasion_id`,`price`,`qty`)
+VALUES
+("Margherita",1,2,10),
+("Viola del pensiero",2,20,8),
+("Rosa",4,20,12),
+("Tulipano",4,35,3),
+("Gerbera",1,3,10),
+("Calla",3,8,20),
+("Orchidea",3,30,5);
+
+
+INSERT INTO `occasions`
+(`name`,`description`)
+VALUES
+("Innamoramento","Quando ci si innamora servono dei fiori per divinare"),
+("Studio","Quando si studia, servono fiori per pensare"),
+("Matrimonio","Tipica usanza di ogni cultura umana, servono fiori per la nuova vita assieme"),
+("Anniversario","Ci vuole un fiore per chiedere scusa per essersi dimenticati l'anniversario");
+
